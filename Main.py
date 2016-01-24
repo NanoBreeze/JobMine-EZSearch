e21d1af510218ac9c46b300e881628788a73fe00
Lenny Cheng <NanoBreeze@users.noreply.github.com>
2016-01-24 04:29:05 -0500

    Finished About page. To do, welcome page and login

--- Main.py
+++ Main.py
@@ -1,3 +1,20 @@
+"""
+This file is part of JobMine EZSearch.
+
+JobMine EZSearch is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+JobMine EZSearch is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with JobMine EZSearch.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from flask import Flask, render_template, request, jsonify
 from post import posts
 from download import downloads
@@ -54,10 +71,10 @@ def analytics_export():
                            analytics_job_containing_keywords_count = analytics.get_keywords_count()
                            )
 
-@app.route('/docs')
+@app.route('/about')
 def docs():
     """Displays the documentation page"""
-    return render_template("Docs.html")
+    return render_template("About.html")
 
 @app.route('/feedback')
 def feedback():
